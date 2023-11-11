de914ebfe5023aad8b1916c90ba244e659a61c9b
Kunal <kunalsingh2703@gmail.com>
2023-11-12 01:42:00 +0530

    update Django Rest Framework

--- api/settings.py
+++ api/settings.py
@@ -126,6 +126,10 @@ STATIC_ROOT = "static"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
 }
